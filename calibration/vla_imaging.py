a83472635319c1dca0110fcc189114a165b5a0aa
kelvinwandia <kelvinw@canopus.jb.man.ac.uk>
2024-05-28 17:55:56 +0100

    Added uvplot

--- calibration/vla_imaging.py
+++ calibration/vla_imaging.py
@@ -2,46 +2,60 @@ import os, glob, re, subprocess
 import numpy as np
 from astropy.constants import c
 import casatools
+from casaplotms import *
 from casatasks import *
 
 
 working_directory = '/raid1/scratch/kelvinw/gcs/m15/measurement_sets/working_dir'
 path_to_vis = '/raid1/scratch/kelvinw/gcs/m15/measurement_sets'
-vis = os.path.join(path_to_vis,'*.ms')
+vis = glob.glob(os.path.join(path_to_vis, '*.ms'))
+
+# vis = '/raid1/scratch/kelvinw/gcs/m15/working_directory_observation.55707.477620949074/11A-269_sb4158083_1.55707.47758597222_hs.ms'
 target = 'M 15 X-2' 
 
 def set_working_dir():
-
     if not os.path.exists(working_directory):
-        # logging.info(f"{working_directory} does not exist, making one")
+        print(f"{working_directory} does not exist, creating it.")
         os.makedirs(working_directory)
     else:
-        # logging.info(f"Working directory {working_directory} already exists")
-        pass
+        print(f"Working directory {working_directory} already exists.")
 
     os.chdir(working_directory)
+    print(f"Current working directory: {os.getcwd()}")
 
 
-def split():
-
+def split_target():
     for msname in vis:
-        outputvis = msname.replace('.ms',f'_{target}_split.ms')
+        outputvis = msname.replace(' ','_').replace('.ms',f'_{target}_split.ms')
+        print(outputvis)
         if not os.path.exists(outputvis):
-            split(vis=outputvis,field=target,datacolumn='corrected')
+            split(vis=msname,outputvis=outputvis,field=target,datacolumn='corrected')
         
         listobs(vis=outputvis,listfile=outputvis.replace('.ms','.txt'),overwrite=True)
 
 
-def getimaging_params():
-    outputvis = msname.replace('.ms',f'_{target}_split.ms')
+def plotuv_cov():
     for msname in vis:
+        plotfile = msname.replace('.ms','_uvcov.png')
+        if not os.path.exists(plotfile):
+            plotms(vis=msname, xaxis='Uwave',yaxis='Vwave',plotfile=plotfile,field=target,
+                    height=750,width=1500,showgui=False,overwrite=True)
+
+
+
+def getimaging_params():
+
+    
+    imaging_params = {}
+    for msname in vis:
+        outputvis = msname.replace(' ','_').replace('.ms',f'_{target}_split.ms')
         ms = casatools.ms()
         tb = casatools.table()
         ms.open(outputvis)
         max_uv = ms.getdata('uvdist')['uvdist'].max()
         ms.close()
 
-        tb.open(vis + '/SPECTRAL_WINDOW')
+        tb.open(outputvis + '/SPECTRAL_WINDOW')
         chan_freq = tb.getcol('CHAN_FREQ')
         highest_freq = chan_freq.max()
         tb.close()
@@ -50,11 +64,13 @@ def getimaging_params():
         # 5 is the sampling
         cell_size = ((c.value / highest_freq) / max_uv) * (180. / np.pi) * (3.6e6 / 5)
         cell_size = np.round(cell_size)
-        logging.info("You are using a cell size of:", cell_size)
+        imaging_params[msname] = cell_size
+        print(imaging_params)
 
- 
 
-    return cell_size
-
+set_working_dir()
+split_target()
+plotuv_cov()
+getimaging_params()
 
 
