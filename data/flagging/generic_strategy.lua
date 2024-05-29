bd51e312f735057971d03d15c63dd1e885b975d5
kelvinwandia <kelvinw@canopus.jb.man.ac.uk>
2024-05-29 07:43:24 +0100

    Use generic strategy not the minimal -- much better flagging

--- data/flagging/generic_strategy.lua
+++ data/flagging/generic_strategy.lua
@@ -1,10 +1,9 @@
 --[[
- This is the generic "minimal" AOFlagger strategy, version 2020-06-14
+ This is the default AOFlagger strategy, version 2021-03-30
  Author: André Offringa
 
- It is functionally equal to the default generic strategy, but removes some of the
- tweaking parameters and visualization. It is therefore less easy to tweak, but smaller
- and possibly slightly faster.
+ This strategy is made as generic / easy to tweak as possible, with the most important
+'tweaking' parameters available as variables at the beginning of function 'execute'.
 ]]
 
 aoflagger.require_min_version("3.0")
@@ -14,12 +13,24 @@ function execute(input)
   -- Generic settings
   --
 
-  local base_threshold = 0.9-- lower means more sensitive detection
+  -- What polarizations to flag? Default: input:get_polarizations() (=all that are in the input data)
+  -- Other options are e.g.:
+  -- { 'XY', 'YX' } to flag only XY and YX, or
+  -- { 'I', 'Q' } to flag only on Stokes I and Q
+  local flag_polarizations = input:get_polarizations()
+
+  local base_threshold = 1.0 -- lower means more sensitive detection
   -- How to flag complex values, options are: phase, amplitude, real, imaginary, complex
-  local representation = "amplitude"
+  -- May have multiple values to perform detection multiple times
+  local flag_representations = { "amplitude" }
   local iteration_count = 3 -- how many iterations to perform?
-  local threshold_factor_step = 2.5 -- How much to increase the sensitivity each iteration?
-  local transient_threshold_factor = 0.9 -- decreasing this value makes detection of transient RFI more aggressive
+  local threshold_factor_step = 2.0 -- How much to increase the sensitivity each iteration?
+  -- If the following variable is true, the strategy will consider existing flags
+  -- as bad data. It will exclude flagged data from detection, and make sure that any existing
+  -- flags on input will be flagged on output. If set to false, existing flags are ignored.
+  local exclude_original_flags = true
+  local frequency_resize_factor = 1.0 -- Amount of "extra" smoothing in frequency direction
+  local transient_threshold_factor = 1.0 -- decreasing this value makes detection of transient RFI more aggressive
 
   --
   -- End of generic settings
@@ -27,87 +38,123 @@ function execute(input)
 
   local inpPolarizations = input:get_polarizations()
 
-  input:clear_mask()
+  if not exclude_original_flags then
+    input:clear_mask()
+  end
+  -- For collecting statistics. Note that this is done after clear_mask(),
+  -- so that the statistics ignore any flags in the input data.
+  local copy_of_input = input:copy()
 
-  for ipol, polarization in ipairs(inpPolarizations) do
-    local converted_data = input:convert_to_polarization(polarization):convert_to_complex(representation)
+  for ipol, polarization in ipairs(flag_polarizations) do
+    local pol_data = input:convert_to_polarization(polarization)
+    local converted_data
+    local converted_copy
 
-    local converted_copy = converted_data:copy()
+    for _, representation in ipairs(flag_representations) do
+      converted_data = pol_data:convert_to_complex(representation)
+      converted_copy = converted_data:copy()
 
-    for i = 1, iteration_count - 1 do
+      for i = 1, iteration_count - 1 do
         local threshold_factor = threshold_factor_step ^ (iteration_count - i)
 
-        local x_threshold_factor = threshold_factor * base_threshold
-        -- Runs sumthreshold and detects sharp, line-shaped features in the time-freq domain
-        -- Parameters
-        -- x_threshold_factor - threshold_factor in time direction
-        -- y_threshold_factor - threshold_factor in frequency direction
-        -- x_direction (bool) - enable flagging in time direction
-        -- y_direction (bool) - enable flagging in frequency direction
-        aoflagger.sumthreshold(converted_data, x_threshold_factor, x_threshold_factor * transient_threshold_factor, true, true)
+        local sumthr_level = threshold_factor * base_threshold
+        if exclude_original_flags then
+          aoflagger.sumthreshold_masked(
+            converted_data,
+            converted_copy,
+            sumthr_level,
+            sumthr_level * transient_threshold_factor,
+            true,
+            true
+          )
+        else
+          aoflagger.sumthreshold(converted_data, sumthr_level, sumthr_level * transient_threshold_factor, true, true)
+        end
 
         -- Do timestep & channel flagging
-        -- Calculates the root mean square RMS for each timestep and flags channels with outlier
         local chdata = converted_data:copy()
         aoflagger.threshold_timestep_rms(converted_data, 3.5)
         aoflagger.threshold_channel_rms(chdata, 3.0 * threshold_factor, true)
         converted_data:join_mask(chdata)
 
-        -- High pass filtering steps 
-        --
-        -- high_pass_filter removes the diffuse background in the resized_data
-        --
-        -- Parameters
-            --  data - the data modified in place
-            --  xsize (integer): kernel size in time direction
-            --  ysize - kernel size in frequency direction
-            --  xsigma - Gaussian width in time direction
-            --  ysigma - Gaussian size in frequency direction
-            --  removes astromomical signals before thresholding
-            --  for high resolution data, use a large kernal either in frequency and/or time
-
-        local xsize = 51
-        local ysize = 51
-        local xsigma = 3.0
-        local ysigma = 3.0
-        
-        --  Downsampling
-        --  Decreases the resolution of the data using simple linear binning - which can increase
-        --  the speed of data smoothing
-        --  Parameters
-            -- data - the input data
-            -- xfactor(int) - the downsampling in time direction
-            -- yfactor(int) - the downsampling in frequency direction
-            -- masked(bool) - take flags into account when averaging
-
-        local time_resize_factor = 1 -- allows convolution with a smaller kernel
-        local frequency_resize_factor = 1
-
+        -- High pass filtering steps
         converted_data:set_visibilities(converted_copy)
-        local resized_data = aoflagger.downsample(converted_data, time_resize_factor, frequency_resize_factor, true)
-        aoflagger.low_pass_filter(resized_data, xsize, ysize, xsigma, ysigma)
-        aoflagger.upsample(resized_data, converted_data, time_resize_factor, frequency_resize_factor)
+        if exclude_original_flags then
+          converted_data:join_mask(converted_copy)
+        end
 
+        local resized_data = aoflagger.downsample(converted_data, 3, frequency_resize_factor, true)
+        aoflagger.low_pass_filter(resized_data, 21, 31, 2.5, 5.0)
+        aoflagger.upsample(resized_data, converted_data, 3, frequency_resize_factor)
+
+        -- In case this script is run from inside rfigui, calling
+        -- the following visualize function will add the current result
+        -- to the list of displayable visualizations.
+        -- If the script is not running inside rfigui, the call is ignored.
+        aoflagger.visualize(converted_data, "Fit #" .. i, i - 1)
 
         local tmp = converted_copy - converted_data
         tmp:set_mask(converted_data)
         converted_data = tmp
 
-        aoflagger.set_progress((ipol - 1) * iteration_count + i, #inpPolarizations * iteration_count)
+        aoflagger.visualize(converted_data, "Residual #" .. i, i + iteration_count)
+        aoflagger.set_progress((ipol - 1) * iteration_count + i, #flag_polarizations * iteration_count)
+      end -- end of iterations
 
-    end -- end of iterations
+      if exclude_original_flags then
+        aoflagger.sumthreshold_masked(
+          converted_data,
+          converted_copy,
+          base_threshold,
+          base_threshold * transient_threshold_factor,
+          true,
+          true
+        )
+      else
+        aoflagger.sumthreshold(converted_data, base_threshold, base_threshold * transient_threshold_factor, true, true)
+      end
+    end -- end of complex representation iteration
 
-    aoflagger.sumthreshold(converted_data, base_threshold, base_threshold * transient_threshold_factor, true, true)
-
-    if input:is_complex() then
-        converted_data = converted_data:convert_to_complex("complex")
+    if exclude_original_flags then
+      converted_data:join_mask(converted_copy)
     end
-    input:set_polarization_data(polarization, converted_data)
 
-    aoflagger.set_progress(ipol, #inpPolarizations)
+    -- Helper function used below
+    function contains(arr, val)
+      for _, v in ipairs(arr) do
+        if v == val then
+          return true
+        end
+      end
+      return false
+    end
+
+    if contains(inpPolarizations, polarization) then
+      if input:is_complex() then
+        converted_data = converted_data:convert_to_complex("complex")
+      end
+      input:set_polarization_data(polarization, converted_data)
+    else
+      input:join_mask(converted_data)
+    end
+
+    aoflagger.visualize(converted_data, "Residual #" .. iteration_count, 2 * iteration_count)
+    aoflagger.set_progress(ipol, #flag_polarizations)
   end -- end of polarization iterations
 
+  if exclude_original_flags then
+    aoflagger.scale_invariant_rank_operator_masked(input, copy_of_input, 0.2, 0.2)
+  else
     aoflagger.scale_invariant_rank_operator(input, 0.2, 0.2)
-    aoflagger.threshold_timestep_rms(input, 4.0)
-    input:flag_nans()
+  end
+
+  aoflagger.threshold_timestep_rms(input, 4.0)
+
+  if input:is_complex() and input:has_metadata() then
+    -- This command will calculate a few statistics like flag% and stddev over
+    -- time, frequency and baseline and write those to the MS. These can be
+    -- visualized with aoqplot.
+    aoflagger.collect_statistics(input, copy_of_input)
+  end
+  input:flag_nans()
 end
