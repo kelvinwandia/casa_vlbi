17aff98d857fffc073e43adc0793710d56a1cd97
kelvinwandia <muriithikangeri@gmail.com>
2025-10-20 20:56:17 +0100

    Fixed timerange error

--- config_file.py
+++ config_file.py
@@ -26,7 +26,7 @@ singularity_container = configuration_file.get('globals','singularity_container'
 # apriori
 use_casa = configuration_file.getboolean('apriori','use_casa')
 attach_metadata = configuration_file.getboolean('apriori','attach_metadata')
-idifitsfiles = configuration_file.get('apriori','idifitsfiles')
+fitsfiles_dir = configuration_file.get('apriori','fitsfiles_dir')
 antab_file = configuration_file.get('apriori','antab_file')
 uvflg_file = configuration_file.get('apriori','uvflg_file')
 do_apriori_cal = configuration_file.getboolean('apriori','do_apriori_cal')
@@ -62,7 +62,6 @@ do_sbd_fringe = configuration_file.getboolean('calibrate','do_sbd_fringe')
 apply_sbd = configuration_file.getboolean('calibrate','apply_sbd')
 refant = configuration_file.get('calibrate','refant')
 timerange = configuration_file.get('calibrate','timerange')
-timerange_ar = configuration_file.get('calibrate','timerange_ar')
 snr_sbd = configuration_file.getfloat('calibrate','snr_sbd')
 do_mbd_fringe = configuration_file.getboolean('calibrate','do_mbd_fringe')
 apply_mbd = configuration_file.getboolean('calibrate','apply_mbd')
@@ -71,43 +70,43 @@ solint = configuration_file.getfloat('calibrate','solint')
 do_bpass = configuration_file.getboolean('calibrate','do_bpass')
 apply_bpass = configuration_file.getboolean('calibrate','apply_bpass')
 
-make_dirty_map = configuration_file.getboolean('calibrate','make_dirty_map')
-split_calibrated = configuration_file.getboolean('calibrate','split_calibrated')
-imsize= [int(part) for part in configuration_file.get('calibrate', 'imsize').split(',')]
-detection_threshold = configuration_file.getfloat('selfcal','detection_threshold')
+# make_dirty_map = configuration_file.getboolean('calibrate','make_dirty_map')
+# split_calibrated = configuration_file.getboolean('calibrate','split_calibrated')
+# imsize= [int(part) for part in configuration_file.get('calibrate', 'imsize').split(',')]
+# detection_threshold = configuration_file.getfloat('selfcal','detection_threshold')
 
-# selfcal
-do_selfcal = configuration_file.getboolean('selfcal','do_selfcal')
-use_tclean = configuration_file.getboolean('selfcal','use_tclean')
-use_wsclean = configuration_file.getboolean('selfcal','use_wsclean')
+# # selfcal
+# do_selfcal = configuration_file.getboolean('selfcal','do_selfcal')
+# use_tclean = configuration_file.getboolean('selfcal','use_tclean')
+# use_wsclean = configuration_file.getboolean('selfcal','use_wsclean')
 
-pybdsf_threshold = configuration_file.get('selfcal','pybdsf_threshold')
-pybdsf_niter = configuration_file.getint('selfcal','pybdsf_niter')
+# pybdsf_threshold = configuration_file.get('selfcal','pybdsf_threshold')
+# pybdsf_niter = configuration_file.getint('selfcal','pybdsf_niter')
 
-weighting = configuration_file.get('selfcal','weighting')
-robust = configuration_file.getfloat('selfcal','robust')
+# weighting = configuration_file.get('selfcal','weighting')
+# robust = configuration_file.getfloat('selfcal','robust')
 
-nloops = configuration_file.getint('selfcal','nloops')
-calmode = configuration_file.get('selfcal','calmode').split(',')
-gaintype = configuration_file.get('selfcal','gaintype').split(',')
-cell =  configuration_file.get('selfcal', 'cell')
-threshold = configuration_file.get('selfcal','threshold').split(',')
-minsnr = [float(part) for part in configuration_file.get('selfcal', 'minsnr').split(',')]
-# imsize= [int(part) for part in configuration_file.get('selfcal', 'imsize').split(',')]
-# niter = [int(part) for part in configuration_file.get('selfcal', 'niter').split(',')]
-niter = int(configuration_file.get('selfcal','niter'))
-niter_final = int(configuration_file.getint('selfcal','niter_final'))
-threshold_final = configuration_file.getint('selfcal','threshold_final')
-robust = configuration_file.getfloat('selfcal','robust')
+# nloops = configuration_file.getint('selfcal','nloops')
+# calmode = configuration_file.get('selfcal','calmode').split(',')
+# gaintype = configuration_file.get('selfcal','gaintype').split(',')
+# cell =  configuration_file.get('selfcal', 'cell')
+# threshold = configuration_file.get('selfcal','threshold').split(',')
+# minsnr = [float(part) for part in configuration_file.get('selfcal', 'minsnr').split(',')]
+# # imsize= [int(part) for part in configuration_file.get('selfcal', 'imsize').split(',')]
+# # niter = [int(part) for part in configuration_file.get('selfcal', 'niter').split(',')]
+# niter = int(configuration_file.get('selfcal','niter'))
+# niter_final = int(configuration_file.getint('selfcal','niter_final'))
+# threshold_final = configuration_file.getint('selfcal','threshold_final')
+# robust = configuration_file.getfloat('selfcal','robust')
 
-tclean_threshold = configuration_file.get('selfcal','tclean_threshold')
-solint_selfcal = configuration_file.get('selfcal','solint_selfcal').split(',')
-apply_to_target = configuration_file.getboolean('selfcal','apply_to_target')
-detect_sources = configuration_file.getboolean('selfcal','detect_sources')
+# tclean_threshold = configuration_file.get('selfcal','tclean_threshold')
+# solint_selfcal = configuration_file.get('selfcal','solint_selfcal').split(',')
+# apply_to_target = configuration_file.getboolean('selfcal','apply_to_target')
+# detect_sources = configuration_file.getboolean('selfcal','detect_sources')
 
-# pb corrections
-do_pbcor = configuration_file.getboolean('pbcor','do_pbcor')
-pb_file = configuration_file.get('pbcor','pb_file')
+# # pb corrections
+# do_pbcor = configuration_file.getboolean('pbcor','do_pbcor')
+# pb_file = configuration_file.get('pbcor','pb_file')
 
 
 vis = experiment+'.ms'
